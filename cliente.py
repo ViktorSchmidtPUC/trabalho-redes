c79b0aca5806705be6544269db63a1bf6147e716
ViktorSchmidtPUC <viktor.schmidt@pucpr.edu.br>
2024-11-18 13:24:40 -0300

    first commit

---
+++ cliente.py
@@ -0,0 +1,39 @@
+import socket
+import threading
+
+# Função principal do cliente
+def cliente_chat():
+    HOST = '127.0.0.1'  # IP do servidor
+    PORTA = 8888             # Porta do servidor
+
+    nome = input("digite seu nome: ")
+    cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    cliente.connect((HOST, PORTA))
+
+    # Enviar nome para o servidor
+    cliente.send(nome.encode())
+
+    # Thread para receber mensagens do servidor
+    def receber_mensagens():
+        while True:
+            try:
+                mensagem = cliente.recv(1024).decode()
+                if mensagem:
+                    print(mensagem)
+            except:
+                print("chat encerrado.")
+                break
+
+    th_receber = threading.Thread(target=receber_mensagens)
+    th_receber.start()
+
+    # Loop para enviar mensagens para o servidor
+    while True:
+        mensagem = input()
+        if mensagem.lower() == "/sair":
+            cliente.close()
+            break
+        cliente.send(mensagem.encode())
+
+# Iniciar o cliente
+cliente_chat()
